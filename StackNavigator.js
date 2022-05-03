e0109cd08b0ac1c873122b708ae6ed61ca050cdc
sam261981 <sam_zo@hotmail.co.uk>
2022-05-03 16:56:51 +0100

    Side Menu

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,25 +1,31 @@
-import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./screens/HomeScreen";
-import TestScreen from "./screens/TestScreen";
-import LoginScreen from "./screens/LoginScreen";
-import { useContext } from "react";
-import { UserContext } from "./contexts/user";
-import ExampleQuestion from "./screens/ExampleQuestion";
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomeScreen from './screens/HomeScreen'
+import TestScreen from './screens/TestScreen'
+import LoginScreen from './screens/LoginScreen'
+import { useContext } from 'react'
+import { UserContext } from './contexts/user'
+import ExampleQuestion from './screens/ExampleQuestion'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
+
+const Drawer = createDrawerNavigator()
 
 const StackNavigator = () => {
-  const { user } = useContext(UserContext);
+  const { user } = useContext(UserContext)
 
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Test" component={TestScreen} />
-      <Stack.Screen name="ExampleQuestion" component={ExampleQuestion} />
-      {user ? <></> : <Stack.Screen name="Login" component={LoginScreen} />}
-    </Stack.Navigator>
-  );
-};
+    <Drawer.Navigator
+      initialRouteName="Home"
+      screenOptions={{ headerShown: true }}
+    >
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="Test" component={TestScreen} />
+      <Drawer.Screen name="ExampleQuestion" component={ExampleQuestion} />
+      {user ? <></> : <Drawer.Screen name="Login" component={LoginScreen} />}
+    </Drawer.Navigator>
+  )
+}
 
-export default StackNavigator;
+export default StackNavigator
